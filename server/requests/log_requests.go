5bcc55765c90eda4a4a8b467a766eb8e7fbffb80
Oleksandr Brichak <aleksandr.brichak@nixsolutions.com>
2020-07-27 16:44:10 +0300

    Add example Log  values for Swagger doc

--- server/requests/log_requests.go
+++ server/requests/log_requests.go
@@ -1,7 +1,7 @@
 package requests
 
 type SaveLogRequest struct {
-	Timestamp string `json:"timestamp"`
-	IP  	  string `json:"ip"`
-	Url 	  string `json:"url"`
+	Timestamp string `json:"timestamp" example:"2020-06-24T15:27:00.123456Z"`
+	IP  	  string `json:"ip" example:"83.150.59.250"`
+	Url 	  string `json:"url" example:"https://example.com/homepage"`
 }
